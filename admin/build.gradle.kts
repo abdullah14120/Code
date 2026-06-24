e0467067678d429d56e1a61547d3d44497befa72
abdullah14120 <abdullah.tamemii@gmail.com>
2026-06-24 15:50:33 +0300

    Add Material Components library dependency

--- admin/build.gradle.kts
+++ admin/build.gradle.kts
@@ -78,6 +78,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
     implementation("com.google.firebase:firebase-database-ktx")
     implementation("com.google.firebase:firebase-storage-ktx")
+    implementation("com.google.android.material:material:1.12.0")
 
     // Testing
     testImplementation("junit:junit:4.13.2")
